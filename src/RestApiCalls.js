e81db72ccbd5d50e67638a2029a7e2c468ad4f8c
gbhatia <gagan3012@users.noreply.github.com>
2020-05-26 10:20:13 +0530

    Api keys updated

--- src/RestApiCalls.js
+++ src/RestApiCalls.js
@@ -1,6 +1,6 @@
 //Load the Latest Quote of the organization data
 export const loadLatestQuote = symbol => {
-  return fetch("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=5min&outputsize=full&apikey=G65K9Q9YE1T9BXFR").then(res => res.json());
+  return fetch("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=5min&outputsize=full&apikey=TSWQZ2043OGTV4T8").then(res => res.json());
 };
 
 //Load the company profile data
@@ -10,7 +10,7 @@ export const getCompanyProfile = symbol => {
 
 //Load the Company Logo
 export const logo = symbol => {
-  return fetch("https://cloud.iexapis.com/v1/stock/" + symbol + "/logo?token=pk_7f40fdc95e4c46268d90695ab47e7b22").then(res => res.json());
+  return fetch("https://cloud.iexapis.com/v1/stock/" + symbol + "/logo?token=pk_409c638849f940088c57402f6c9d49ad ").then(res => res.json());
 };
 
 //Load the Sector Performance data
@@ -34,10 +34,10 @@ export const getFullHistoricalData = symbol => {
 
 //Load last 5 days data of stock price
 export const getFiveDaysPrice = symbol => {
-  return fetch("https://cloud.iexapis.com/stable/stock/" + symbol + "/chart/5d?token=pk_1cd7010654304d9da845c98f9e030ab8").then(res => res.json());
+  return fetch("https://cloud.iexapis.com/stable/stock/" + symbol + "/chart/5d?token=pk_409c638849f940088c57402f6c9d49ad ").then(res => res.json());
 }
 
 //Load latest news of company
 export const getNews = symbol => {
-  return fetch("https://cloud.iexapis.com/stable/stock/" + symbol + "/news/last/5?token=pk_11795e9e241a4c278aee46443660ba6d").then(res => res.json());
+  return fetch("https://cloud.iexapis.com/stable/stock/" + symbol + "/news/last/5?token=pk_409c638849f940088c57402f6c9d49ad ").then(res => res.json());
 }
