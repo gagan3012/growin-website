cd24730cf7f479660ec4dc57749e422799db44fd
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2020-05-26 21:04:54 +0530

    Update RestApiCalls.js

--- src/RestApiCalls.js
+++ src/RestApiCalls.js
@@ -5,17 +5,17 @@ export const loadLatestQuote = symbol => {
 
 //Load the company profile data
 export const getCompanyProfile = symbol => {
-  return fetch("https://financialmodelingprep.com/api/v3/company/profile/" + symbol + "?apikey=5696dbf21c11c436193231d0a30f538f").then(res => res.json());
+  return fetch("https://financialmodelingprep.com/api/v3/company/profile/" + symbol + "?apikey=6282f3aac7873b82f1c883b41c51c17e").then(res => res.json());
 };
 
 //Load the Company Logo
 export const logo = symbol => {
-  return fetch("https://cloud.iexapis.com/v1/stock/" + symbol + "/logo?token=pk_7f40fdc95e4c46268d90695ab47e7b22").then(res => res.json());
+  return fetch("https://cloud.iexapis.com/v1/stock/" + symbol + "/logo?token=pk_409c638849f940088c57402f6c9d49ad ").then(res => res.json());
 };
 
 //Load the Sector Performance data
 export const sector_perf = () => {
-  return fetch("https://financialmodelingprep.com/api/v3/stock/sectors-performance?apikey=5696dbf21c11c436193231d0a30f538f").then(res => res.json());
+  return fetch("https://financialmodelingprep.com/api/v3/stock/sectors-performance?apikey=6282f3aac7873b82f1c883b41c51c17e").then(res => res.json());
 };
 
 export const trending_now = () => {
@@ -29,7 +29,7 @@ export const symbols_company = () => {
 
 //Load full historial data (last 5 years data) of stock price
 export const getFullHistoricalData = symbol => {
-  return fetch("https://financialmodelingprep.com/api/v3/historical-price-full/" + symbol + "?apikey=5696dbf21c11c436193231d0a30f538f").then(res => res.json());
+  return fetch("https://financialmodelingprep.com/api/v3/historical-price-full/" + symbol + "?apikey=6282f3aac7873b82f1c883b41c51c17e").then(res => res.json());
 };
 
 //Load last 5 days data of stock price
@@ -38,11 +38,11 @@ export const getFiveDaysPrice = symbol => {
     'Content-Type': 'application/json',
         'Accept': 'application/json'
   }}
-  return fetch("https://cloud.iexapis.com/stable/stock/" + symbol + "/chart/5d?token=pk_1cd7010654304d9da845c98f9e030ab8", opts).then(res =>{
+  return fetch("https://cloud.iexapis.com/stable/stock/" + symbol + "/chart/5d?token=pk_409c638849f940088c57402f6c9d49ad ", opts).then(res =>{
       console.log(res); res.json()});
 }
 
 //Load latest news of company
 export const getNews = symbol => {
-  return fetch("https://cloud.iexapis.com/stable/stock/" + symbol + "/news/last/5?token=pk_11795e9e241a4c278aee46443660ba6d").then(res => res.json());
+  return fetch("https://cloud.iexapis.com/stable/stock/" + symbol + "/news/last/5?token=pk_409c638849f940088c57402f6c9d49ad ").then(res => res.json());
 }
